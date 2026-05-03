77d98df2b5f81819e96264a8d7a2d35360c91196
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-02 23:23:24 -0700

    feat: setup vpc and networking modules

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,6 @@
+variable "project_name" {}
+variable "cluster_name" {}
+variable "vpc_cidr" {}
+variable "public_subnet_cidrs" {}
+variable "private_subnet_cidrs" {}
+variable "availability_zones" {}
