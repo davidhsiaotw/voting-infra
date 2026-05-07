0f5f3c26390abb7e2e6d16811f15eb8f817a8644
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-07 15:14:41 -0700

    feat: introduce GitOps with ArgoCD, Argo Rollouts, and Kustomize
    add base Kubernetes manifests and Kustomize configurations for all voting application components (db, frontend, redis, result, vote, worker)
    implement Argo Rollouts for the `vote` component to enable canary deployment strategies
    create Kustomize overlays for `dev`, `uat`, and `prod` environments, including environment-specific labels, prefixes, and secret generators.
    add ArgoCD Application manifests (`argocd-apps.yaml`) to automatically sync the Kustomize overlays for each environment
    update Terraform `provider.tf` to include and configure `kubernetes` and `helm` providers utilizing the AWS EKS cluster data source
    add a new `k8s-addons` Terraform module to automatically provision ArgoCD and Argo Rollouts into the cluster via Helm

---
+++ modules/k8s-addons/main.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  version    = "5.46.7"
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+}
+
+resource "helm_release" "argo_rollouts" {
+  name       = "argo-rollouts"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-rollouts"
+  namespace  = "kube-system"
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
--- provider.tf
+++ provider.tf
@@ -6,9 +6,43 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
 }
