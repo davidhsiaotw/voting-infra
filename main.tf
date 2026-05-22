569286512276d8bd308655ef982fe9957dcb4700
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-21 17:34:35 -0700

    feat: create VPC, EKS, RDS via Terraform

---
+++ main.tf
@@ -0,0 +1,26 @@
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
+data "aws_ssm_parameter" "db_password" {
+  name            = "voting-db-password"
+  with_decryption = true
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "eks" {
+  source       = "./modules/eks"
+  subnet_ids   = module.vpc.public_subnet_ids
+  lab_role_arn = data.aws_iam_role.lab_role.arn
+}
+
+module "rds" {
+  source      = "./modules/rds"
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.public_subnet_ids
+  vpc_cidr    = module.vpc.vpc_cidr_block
+  db_password = data.aws_ssm_parameter.db_password.value
+}
---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  description = "Endpoint for EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "rds_endpoint" {
+  description = "Endpoint for RDS instance"
+  value       = module.rds.db_endpoint
+}
