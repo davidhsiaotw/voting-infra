5a42ffdc9b950d9102d24aa2dfd1811dcedff8a4
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-21 17:33:11 -0700

    feat: add VPC TF module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+  description = "Availability zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
