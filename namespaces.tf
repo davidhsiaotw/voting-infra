3c38535c5e79613d0b3fe25772e19877c1d4d024
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-24 21:59:14 -0700

    feat: add database endpoint and password into Kubternetes Secrets

--- namespaces.tf
+++ namespaces.tf
@@ -18,3 +18,50 @@ resource "kubernetes_namespace" "prod" {
   }
   depends_on = [module.eks]
 }
+
+# Extract the hostname by removing the port from the RDS endpoint
+locals {
+  db_endpoint_hostname = split(":", module.rds.db_endpoint)[0]
+}
+
+resource "kubernetes_secret" "db_credentials_dev" {
+  metadata {
+    name      = "db-credentials"
+    namespace = kubernetes_namespace.dev.metadata[0].name
+  }
+
+  data = {
+    DB_ENDPOINT = local.db_endpoint_hostname
+    DB_PASSWORD = data.aws_ssm_parameter.db_password.value
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_secret" "db_credentials_uat" {
+  metadata {
+    name      = "db-credentials"
+    namespace = kubernetes_namespace.uat.metadata[0].name
+  }
+
+  data = {
+    DB_ENDPOINT = local.db_endpoint_hostname
+    DB_PASSWORD = data.aws_ssm_parameter.db_password.value
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_secret" "db_credentials_prod" {
+  metadata {
+    name      = "db-credentials"
+    namespace = kubernetes_namespace.prod.metadata[0].name
+  }
+
+  data = {
+    DB_ENDPOINT = local.db_endpoint_hostname
+    DB_PASSWORD = data.aws_ssm_parameter.db_password.value
+  }
+
+  type = "Opaque"
+}
