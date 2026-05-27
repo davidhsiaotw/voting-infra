0cc644a259c32edf32aabee8d6246df901e8dec5
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-26 21:01:54 -0700

    feat: install ExternelDNS on EKS

---
+++ external-dns.tf
@@ -0,0 +1,43 @@
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  namespace  = "kube-system"
+
+  set {
+    name  = "provider"
+    value = "aws"
+  }
+
+  set {
+    name  = "domainFilters[0]"
+    value = "wyxiao.games"
+  }
+
+  set {
+    name  = "policy"
+    value = "sync" # Allows external-dns to delete records it previously created
+  }
+
+  set {
+    name  = "registry"
+    value = "txt"
+  }
+
+  set {
+    name  = "txtOwnerId"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = var.external_dns_iam_role_arn
+  }
+
+  depends_on = [module.eks]
+}
