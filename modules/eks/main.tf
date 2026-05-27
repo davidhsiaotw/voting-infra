cebec54b6b1633c695ad8243f8f6f392c308e93d
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-27 16:08:31 -0700

    feat: scale up EKS to three nodes

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -15,8 +15,8 @@ resource "aws_eks_node_group" "main" {
   instance_types  = ["t3.medium"]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 3
+    max_size     = 4
     min_size     = 2
   }
 
