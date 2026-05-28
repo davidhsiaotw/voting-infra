9a6d3412fbf401e48bb34f0837a29d1029020c96
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-05-27 21:50:26 -0700

    feat: add UAT and PROD ingress with Route53 routing

--- ingress.tf
+++ ingress.tf
@@ -55,6 +55,120 @@ resource "aws_route53_record" "dev" {
   }
 }
 
+# --- UAT Environment ---
+resource "kubernetes_ingress_v1" "voting_app_uat" {
+  metadata {
+    name      = "voting-app-ingress"
+    namespace = kubernetes_namespace.uat.metadata[0].name
+    annotations = {
+      "alb.ingress.kubernetes.io/scheme"          = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type"     = "ip"
+      "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTP\": 80}, {\"HTTPS\": 443}]"
+      "alb.ingress.kubernetes.io/ssl-redirect"    = "443"
+      "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.wildcard.arn
+      "alb.ingress.kubernetes.io/group.name"      = "voting-app-group"
+    }
+  }
+
+  spec {
+    ingress_class_name = "alb"
+    rule {
+      host = "uat.wyxiao.games"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "frontend"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  wait_for_load_balancer = true
+
+  depends_on = [
+    helm_release.aws_load_balancer_controller,
+    aws_acm_certificate_validation.main
+  ]
+}
+
+resource "aws_route53_record" "uat" {
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.main.zone_id
+  name            = "uat.wyxiao.games"
+  type            = "A"
+
+  alias {
+    name                   = kubernetes_ingress_v1.voting_app_uat.status[0].load_balancer[0].ingress[0].hostname
+    zone_id                = "Z35SXDOTRQ7X7K" # ALB Hosted Zone ID for us-east-1
+    evaluate_target_health = true
+  }
+}
+
+# --- PROD Environment ---
+resource "kubernetes_ingress_v1" "voting_app_prod" {
+  metadata {
+    name      = "voting-app-ingress"
+    namespace = kubernetes_namespace.prod.metadata[0].name
+    annotations = {
+      "alb.ingress.kubernetes.io/scheme"          = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type"     = "ip"
+      "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTP\": 80}, {\"HTTPS\": 443}]"
+      "alb.ingress.kubernetes.io/ssl-redirect"    = "443"
+      "alb.ingress.kubernetes.io/certificate-arn" = aws_acm_certificate.wildcard.arn
+      "alb.ingress.kubernetes.io/group.name"      = "voting-app-group"
+    }
+  }
+
+  spec {
+    ingress_class_name = "alb"
+    rule {
+      host = "wyxiao.games"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "frontend"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  wait_for_load_balancer = true
+
+  depends_on = [
+    helm_release.aws_load_balancer_controller,
+    aws_acm_certificate_validation.main
+  ]
+}
+
+resource "aws_route53_record" "prod" {
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.main.zone_id
+  name            = "wyxiao.games"
+  type            = "A"
+
+  alias {
+    name                   = kubernetes_ingress_v1.voting_app_prod.status[0].load_balancer[0].ingress[0].hostname
+    zone_id                = "Z35SXDOTRQ7X7K" # ALB Hosted Zone ID for us-east-1
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "grafana" {
   allow_overwrite = true
   zone_id         = data.aws_route53_zone.main.zone_id
